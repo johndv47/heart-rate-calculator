91574fbdc53bffd591716d082f4e3270c2d92fe7
John Pinto <159508244+johndv47@users.noreply.github.com>
2026-01-16 01:18:03 -0700

    Add target heart rate calculation based on age

---
+++ main.py
@@ -0,0 +1,9 @@
+age = int(input("What is your age?: "))
+
+max_rate = 220 - age
+
+min_rate = max_rate * .50
+
+max_rate = max_rate * .85
+
+target_rate = print(f"Your target heart rate range is {min_rate} - {max_rate}.")
