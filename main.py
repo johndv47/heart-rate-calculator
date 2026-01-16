25e1f0b1f2069a3e65801160c79adfb223599a41
John Pinto <159508244+johndv47@users.noreply.github.com>
2026-01-16 07:45:45 -0700

    Refactor heart rate calculation logic

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+'''
+script: heart_rate_calculator.py
+action: a. request user input for age.
+        b. perform max heart rate calculation.
+        c. perform target min & max heart rate calculation.
+        c. print range of target heart rate.
+author: John Pinto
+date: 1/16/2026
+'''
+
 age = int(input("What is your age?: "))
-
 max_rate = 220 - age
-
-min_rate = max_rate * .50
-
-max_rate = max_rate * .85
-
-target_rate = print(f"Your target heart rate range is {min_rate} - {max_rate}.")
+tgt_min_rate = max_rate * .50
+tgt_max_rate = max_rate * .85
+target_rate = print(f"Your target heart rate range is {tgt_min_rate} - {tgt_max_rate}.")
