6460e27b297e2b95ba7a0c60932502b1e8602ef0
John Pinto <159508244+johndv47@users.noreply.github.com>
2026-01-19 09:25:39 -0700

    Refactor heart rate calculator for clarity and accuracy

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 '''
 script: heart_rate_calculator.py
-action: a. request user input for age.
+action: a. request user input for their age.
         b. perform max heart rate calculation.
-        c. perform target min & max heart rate calculation.
-        c. print range of target heart rate.
+        c. print maximum heart rate.
+        d. perform target min & max heart rate calculation.
+        e. print range of target heart rate.
 author: John Pinto
-date: 1/16/2026
+date: 1/19/2026
 '''
+
+print("Welcome to the heart rate calculator!\n")
 
 age = int(input("What is your age?: "))
 max_rate = 220 - age
-tgt_min_rate = max_rate * .50
-tgt_max_rate = max_rate * .85
-target_rate = print(f"Your target heart rate range is {tgt_min_rate} - {tgt_max_rate}.")
+print(f"\nThe maximum heart rate based on your age is: {max_rate} BPM")
+
+tgt_min_rate = int(max_rate * .50)
+tgt_max_rate = int(max_rate * .85)
+print(f"Your target heart rate range is {tgt_min_rate} - {tgt_max_rate} BPM.")
